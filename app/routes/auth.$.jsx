769cef4f61ee976d08940847646f64bb4022f708
Sérgio Vorniches <vorniches@gmail.com>
2025-04-22 17:01:50 +0100

    widget block installation

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,8 +1,25 @@
-import { authenticate } from "../shopify.server";
-import { insertAppBlock } from "../utils/insertAppBlock.server";
+import shopify from '../shopify.server';
 
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
-  await insertAppBlock(session);
+
+  const client = new shopify.api.rest.Asset({ session });
+
+  const themes = await shopify.api.rest.Theme.all({ session });
+  const mainTheme = themes.find((t) => t.role === 'main');
+
+  const asset = await shopify.api.rest.Asset.update({
+    session,
+    theme_id: mainTheme.id,
+    key: 'templates/index.json',
+    value: JSON.stringify({
+      sections: {
+        main: { type: 'main' },
+        'app-block': { type: 'apps.my-app-block.app-window' }
+      },
+      order: ['app-block', 'main']
+    })
+  });
+
   return null;
 };
